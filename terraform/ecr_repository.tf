612fa20be67118709bc1fb30be256b1dcfe09c24
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 17:38:17 +0100

    Delete terraform/ecr_repository.tf

--- terraform/ecr_repository.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "app_repo" {
-  name = "simple-java-app"
-  image_tag_mutability = "MUTABLE"
-  tags = { Name = "simple-java-app" }
-}
-
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
