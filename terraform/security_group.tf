5dd5c2750ffd508c1bbd7ba358d07f4ae241bf44
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 16:57:17 +0100

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,37 +1,37 @@
 resource "aws_security_group" "alb_sg" {
-  name = "alb-sg"
+  name   = "alb-sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name = "ecs-sg"
+  name   = "ecs-sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
