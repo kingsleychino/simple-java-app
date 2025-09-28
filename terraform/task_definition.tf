5e0f5a1b2e21c1d0758af9576ab98ccbc917a2f3
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 16:58:39 +0100

    Update task_definition.tf

--- terraform/task_definition.tf
+++ terraform/task_definition.tf
@@ -1,13 +1,17 @@
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
+
 resource "aws_ecs_task_definition" "java_app" {
   family                   = "java-app"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
-    name      = "java-app-container"
+    name  = "java-app-container"
     image     = "${var.ecr_repo_url}:${var.image_tag}"   # ✅ dynamic
     essential = true
     portMappings = [{
