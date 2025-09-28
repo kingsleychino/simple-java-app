0498ac9be161ec43002d92be488d84c6e25adcf8
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 17:41:14 +0100

    Delete terraform/output.tf

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  value = aws_lb.app_lb.dns_name
-}
-
-
-output "ecs_service_name" {
-  description = "Name of the ECS service"
-  value       = aws_ecs_service.java_app_service.name
-}
