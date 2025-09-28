66f532b7ded95cd70164e51c110bcb50bc4e2506
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 12:52:32 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "ecr_repo_url" {
+  type        = string
+  description = "ECR repo URL"
+}
+
+variable "image_tag" {
+  type        = string
+  description = "Docker image tag"
+}
