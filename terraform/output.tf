e6a50c11c17ac70416d101db91f29c705753fb23
kingsleychino <kingsleychinono@gmail.com>
2025-09-28 16:56:37 +0100

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,8 @@ output "alb_dns_name" {
   value = aws_lb.app_lb.dns_name
 }
 
+
 output "ecs_service_name" {
-  value = aws_ecs_service.java_service.name
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.java_app_service.name
 }
