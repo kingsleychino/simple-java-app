40c590a91357b03ff3832ac598eea3b09b1cba3c
kingsleychino <kingsleychinono@gmail.com>
2025-09-30 17:40:40 +0000

    updated

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "app_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
