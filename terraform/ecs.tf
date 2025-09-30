569466ea96228bd22a5833e921435b4ac7fb5321
kingsleychino <kingsleychinono@gmail.com>
2025-09-30 10:17:33 +0000

    updated

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -23,13 +23,13 @@ resource "aws_ecs_task_definition" "app_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "java-app-container"
-      //image     = "503499294473.dkr.ecr.us-east-1.amazonaws.com/simple-java-app:build-8"
-      image     = "${var.ecr_repo_url}:${var.image_tag}"
+      name = "java-app-container"
+      image     = "503499294473.dkr.ecr.us-east-1.amazonaws.com/simple-java-app:build-14"
+      //image     = "${var.ecr_repo_url}:${var.image_tag}"
       essential = true
 
       portMappings = [
@@ -62,7 +62,7 @@ resource "aws_ecs_service" "app_service" {
   launch_type     = "FARGATE"
   desired_count   = 1
 
-  
+
 
   network_configuration {
     subnets          = [aws_subnet.public_a.id, aws_subnet.public_b.id]
@@ -77,4 +77,52 @@ resource "aws_ecs_service" "app_service" {
   }
 
   depends_on = [aws_lb_listener.app_listener]
+}
+
+############################################
+# Application Load Balancer
+############################################
+resource "aws_lb" "app_lb" {
+  name               = "java-app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+############################################
+# Target Group
+############################################
+resource "aws_lb_target_group" "app_tg" {
+  name     = "java-app-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+############################################
+# Listener
+############################################
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+
+  depends_on = [aws_lb_target_group.app_tg]
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,8 @@
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = data.aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
