41c439d419007dce846b1ae1bee648bad67d347f
kingsleychino <kingsleychinono@gmail.com>
2025-09-30 17:39:29 +0000

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,10 @@
+variable "ecr_repo_url" {
+  description = "ECR repository URL"
+  type        = string
+}
+
 variable "region" {
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
-  description = "AWS region"
-}
-
-variable "image_tag" {
-  type    = string
-  default = "latest"
-}
-
-variable "ecr_repo_url" {
-  type    = string
-  default = "503499294473.dkr.ecr.us-east-1.amazonaws.com/simple-java-app"
 }
